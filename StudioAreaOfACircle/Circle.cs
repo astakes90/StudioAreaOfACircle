4ed40297127cebac67d9875a2d884c34f1c2f694
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2022-08-03 20:27:32 -0500

    studio 1 complete

---
+++ StudioAreaOfACircle/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace StudioAreaOfACircle
+{
+    public class Circle
+    {
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static double CircumferenceOfCircle(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public static double DiameterOfCircle(double radius)
+        {
+            return 2 * radius;
+        }
+
+        public static double TotalGallonsForTrip(double circumference, double milesPerGallon)
+        {
+            return circumference / milesPerGallon;
+        }
+
+    }
+}
---
+++ StudioAreaOfACircle/Program.cs
@@ -0,0 +1,29 @@
+﻿using StudioAreaOfACircle;
+
+Circle circle = new Circle();
+
+Console.WriteLine("Enter a radius: ");
+
+string userInput = Console.ReadLine();
+
+double radius = double.Parse(userInput);
+
+while (radius <= 1)
+{
+    Console.WriteLine("Enter a valid radius: ");
+    radius = double.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"The area of a circle of radius {radius} is: {Circle.AreaOfCircle(radius)}");
+
+Console.WriteLine($"The circumference of a circle of radius {radius} is: {Circle.CircumferenceOfCircle(radius)}");
+
+Console.WriteLine($"The diameter of a circle of radius {radius} is: {Circle.DiameterOfCircle(radius)}");
+
+Console.WriteLine("Enter your miles per gallon: ");
+
+string userInput2 = Console.ReadLine();
+
+double milesPerGallon = double.Parse(userInput2);
+
+Console.WriteLine($"It will take {Circle.TotalGallonsForTrip(Circle.CircumferenceOfCircle(radius), milesPerGallon)} to get around the circle.");
